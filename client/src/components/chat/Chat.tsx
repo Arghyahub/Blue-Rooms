2854cf34cf2d7f7a919201c7ee1a799be5c318d3
Arghya <arghyadas242004@gmail.com>
2023-08-04 16:54:24 +0530

    Add: Auto scroll down

--- client/src/components/chat/Chat.tsx
+++ client/src/components/chat/Chat.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useRef } from 'react'
 import { useRecoilValue } from 'recoil'
 
 import './Chat.css'
@@ -7,9 +8,19 @@ import { chatProp } from './chatTypes'
 
 const Chat:React.FC<chatProp> = ({name}):JSX.Element => {
   const selectedChat = useRecoilValue(currOpenChat) ;
+  const chatBody = useRef(null) ;
+  
+  useEffect(() => {
+    if (chatBody.current) {
+      const divElem:HTMLDivElement = chatBody.current ;
+      console.log(divElem) ;
+      divElem.scrollTop = divElem.scrollHeight - divElem.clientHeight ;
+    }
+  }, [selectedChat])
+  
 
   return (
-    <div id="Chat">
+    <div id="Chat" ref={chatBody} >
       {selectedChat.selected? (
         <div className="selected-chat flcol">
           <p className='head-name'>{selectedChat.name}</p>
